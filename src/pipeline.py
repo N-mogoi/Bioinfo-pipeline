70f8f771c5fba86c1879e418813fbb1a664ebd37
N-mogoi <mogoinn67@gmail.com>
2026-03-31 09:07:47 +0200

    Add variant analysis step in pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,3 +1,11 @@
 # pipeline.py
-def main():
-    print("Bioinformatics pipeline is running.")
+# Quality Control Step
+
+    # Variant Analysis Step
+def variant_analysis(data):
+    print("Performing variant analysis...")
+    if len(data) < 20:
+        print("Error: Not enough data for variant analysis.")
+    else:
+        print("Variant analysis successful!")
+    print("Variant analysis complete.")
